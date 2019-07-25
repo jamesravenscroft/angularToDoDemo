56b68899c050e53c15d3cc38b69a04298790eb85
James Ravenscroft <44760378+jamesravenscroft@users.noreply.github.com>
2019-07-25 16:43:20 -0700

    Manual Upload

---
+++ src/app/todo/todo.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+
+const TODOS = [
+  { title: 'Install Angular CLI', isDone: true },
+  { title: 'Style app', isDone: true },
+  { title: 'Finish service functionality', isDone: false },
+  { title: 'Setup API', isDone: false },
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  constructor() { }
+
+  get() {
+    return new Promise(resolve => resolve(TODOS));
+  }
+  add(data) {
+    return new Promise(resolve => {
+      TODOS.push(data);
+      resolve(data);
+    });
+  }
+  put(changed) {
+    return new Promise(resolve => {
+      const index = TODOS.findIndex(todo => todo === changed);
+      TODOS[index].title = changed.title;
+      resolve(changed);
+    });
+  }
+
+  delete(selected) {
+    return new Promise(resolve => {
+      const index = TODOS.findIndex(todo => todo === selected);
+      TODOS.splice(index, 1);
+      resolve(true);
+    });
+  }
+
+}
+
+
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class TodoService {
+
+//   constructor() { }
+// }
